3de0b5e914d5b10992227cd905241bfef39d1b83
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-13 23:14:01 -0300

    feat: ensure listToolsUseCase return tools if HttpClient return 200

---
+++ src/domain/models/tools.ts
@@ -0,0 +1,9 @@
+export type Tool = {
+  id: number
+  name: string
+  description: string
+  status: string
+  dateOfCollection: Date
+  dateOfDevolution: Date
+  mechanicName: string
+}
--- src/domain/use-cases/tools/list-tools.ts
+++ src/domain/use-cases/tools/list-tools.ts
@@ -1,14 +1,15 @@
 import { type HttpClient } from '@/domain/contracts/http'
 import { UnexpectedError } from '../../errors'
+import { type Tool } from '../../models'
 
-type Setup = (url: string, httpClient: HttpClient<boolean>) => ListTools
-type Output = void
+type Setup = (url: string, httpClient: HttpClient<Tool[]>) => ListTools
+type Output = Tool[]
 export type ListTools = () => Promise<Output>
 
 export const listToolsUseCase: Setup = (url, httpClient) => async () => {
-  const { statusCode } = await httpClient.request({ url, method: 'get' })
+  const { statusCode, data } = await httpClient.request({ url, method: 'get' })
   switch (statusCode) {
-    case 200: return undefined
+    case 200: return data!
     default: throw new UnexpectedError()
   }
 }
