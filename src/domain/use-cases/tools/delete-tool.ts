282566ea902149ec4effa2535f38fad7c07ce97f
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-13 23:51:49 -0300

    feat: ensure deleteToolUseCase return true if HttpClient return 204

--- src/domain/use-cases/tools/delete-tool.ts
+++ src/domain/use-cases/tools/delete-tool.ts
@@ -3,13 +3,13 @@ import { UnexpectedError } from '../../errors'
 
 type Setup = (url: string, httpClient: HttpClient) => DeleteTool
 type Input = { id: number }
-type Output = void
+type Output = boolean
 export type DeleteTool = (input: Input) => Promise<Output>
 
 export const deleteToolUseCase: Setup = (url, httpClient) => async ({ id }) => {
   const { statusCode } = await httpClient.request({ url: `${url}/${id}`, method: 'delete' })
   switch (statusCode) {
-    case 204: return undefined
+    case 204: return true
     default: throw new UnexpectedError()
   }
 }
