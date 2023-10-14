e94566f7af496d9ba61151bb9ce43ac3569f93bf
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-13 23:41:55 -0300

    test: ensure loadToolByIdUseCase throw UnexpectedError if HttpClient return 500

--- tests/domain/use-cases/tools/load-tool-by-id.spec.ts
+++ tests/domain/use-cases/tools/load-tool-by-id.spec.ts
@@ -1,14 +1,19 @@
 import { type LoadToolById, loadToolByIdUseCase } from '@/domain/use-cases/tools'
 import { type HttpClient } from '@/domain/contracts/http'
 import { httpClientParams, toolsParams } from '@/tests/mocks'
+import { UnexpectedError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
 
 describe('loadToolByIdUseCase', () => {
   let sut: LoadToolById
   const { url } = httpClientParams
-  const { id } = toolsParams
+  const { dateOfCollection, description, dateOfDevolution, id, mechanicName, name, status } = toolsParams
   const httpClient = mock<HttpClient>()
+
+  beforeAll(() => {
+    httpClient.request.mockResolvedValue({ statusCode: 200, data: { dateOfCollection, description, dateOfDevolution, id, mechanicName, name, status } })
+  })
 
   beforeEach(() => {
     sut = loadToolByIdUseCase(url, httpClient)
@@ -19,5 +24,13 @@ describe('loadToolByIdUseCase', () => {
 
     expect(httpClient.request).toHaveBeenCalledWith({ url: `${url}/${id}`, method: 'get' })
     expect(httpClient.request).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw UnexpectedError if HttpClient return 500', async () => {
+    httpClient.request.mockResolvedValueOnce({ statusCode: 500 })
+
+    const promise = sut({ id })
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
