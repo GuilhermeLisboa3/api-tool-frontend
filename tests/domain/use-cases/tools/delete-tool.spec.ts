da8e86cb07248d8203c12783a00572d511477639
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-13 23:50:25 -0300

    test: ensure deleteToolUseCase throw UnexpectedError if HttpClient return 500

--- tests/domain/use-cases/tools/delete-tool.spec.ts
+++ tests/domain/use-cases/tools/delete-tool.spec.ts
@@ -1,6 +1,7 @@
 import { type DeleteTool, deleteToolUseCase } from '@/domain/use-cases/tools'
 import { type HttpClient } from '@/domain/contracts/http'
 import { httpClientParams, toolsParams } from '@/tests/mocks'
+import { UnexpectedError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
 
@@ -9,6 +10,10 @@ describe('deleteToolUseCase', () => {
   const { url } = httpClientParams
   const { id } = toolsParams
   const httpClient = mock<HttpClient>()
+
+  beforeAll(() => {
+    httpClient.request.mockResolvedValue({ statusCode: 204, data: null })
+  })
 
   beforeEach(() => {
     sut = deleteToolUseCase(url, httpClient)
@@ -19,5 +24,13 @@ describe('deleteToolUseCase', () => {
 
     expect(httpClient.request).toHaveBeenCalledWith({ url: `${url}/${id}`, method: 'delete' })
     expect(httpClient.request).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw UnexpectedError if HttpClient return 500', async () => {
+    httpClient.request.mockResolvedValueOnce({ statusCode: 500 })
+
+    const promise = sut({ id })
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
