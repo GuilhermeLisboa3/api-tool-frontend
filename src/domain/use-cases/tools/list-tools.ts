632617b34303a2fcba8300f14ef0551d4c241073
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-13 23:09:49 -0300

    feat: ensure listToolsUseCase throw UnexpectedError if HttpClient return 500

--- src/domain/use-cases/tools/list-tools.ts
+++ src/domain/use-cases/tools/list-tools.ts
@@ -1,9 +1,14 @@
 import { type HttpClient } from '@/domain/contracts/http'
+import { UnexpectedError } from '../../errors'
 
 type Setup = (url: string, httpClient: HttpClient<boolean>) => ListTools
 type Output = void
 export type ListTools = () => Promise<Output>
 
 export const listToolsUseCase: Setup = (url, httpClient) => async () => {
-  await httpClient.request({ url, method: 'get' })
+  const { statusCode } = await httpClient.request({ url, method: 'get' })
+  switch (statusCode) {
+    case 200: return undefined
+    default: throw new UnexpectedError()
+  }
 }
