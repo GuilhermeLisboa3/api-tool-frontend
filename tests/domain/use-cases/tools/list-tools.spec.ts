a22745a0ae3fab1a30d3f7772cee80b030166cc4
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-13 23:10:19 -0300

    test: ensure listToolsUseCase throw UnexpectedError if HttpClient return 500

--- tests/domain/use-cases/tools/list-tools.spec.ts
+++ tests/domain/use-cases/tools/list-tools.spec.ts
@@ -1,6 +1,7 @@
 import { type ListTools, listToolsUseCase } from '@/domain/use-cases/tools'
 import { type HttpClient } from '@/domain/contracts/http'
 import { httpClientParams } from '@/tests/mocks'
+import { UnexpectedError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
 
@@ -8,6 +9,10 @@ describe('listToolsUseCase', () => {
   let sut: ListTools
   const { url } = httpClientParams
   const httpClient = mock<HttpClient>()
+
+  beforeAll(() => {
+    httpClient.request.mockResolvedValue({ statusCode: 200, data: null })
+  })
 
   beforeEach(() => {
     sut = listToolsUseCase(url, httpClient)
@@ -18,5 +23,13 @@ describe('listToolsUseCase', () => {
 
     expect(httpClient.request).toHaveBeenCalledWith({ url, method: 'get' })
     expect(httpClient.request).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw UnexpectedError if HttpClient return 500', async () => {
+    httpClient.request.mockResolvedValueOnce({ statusCode: 500 })
+
+    const promise = sut()
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
