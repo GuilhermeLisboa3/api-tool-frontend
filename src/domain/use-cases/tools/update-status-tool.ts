524b226096983cbe368234d56c0083743b07ce40
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-14 00:03:02 -0300

    feat: ensure updateStatusToolUseCase return true if HttpClient return 204

--- src/domain/use-cases/tools/update-status-tool.ts
+++ src/domain/use-cases/tools/update-status-tool.ts
@@ -3,13 +3,13 @@ import { UnexpectedError } from '../../errors'
 
 type Setup = (url: string, httpClient: HttpClient) => UpdateStatusTool
 type Input = { id: number }
-type Output = void
+type Output = boolean
 export type UpdateStatusTool = (input: Input) => Promise<Output>
 
 export const updateStatusToolUseCase: Setup = (url, httpClient) => async ({ id }) => {
   const { statusCode } = await httpClient.request({ url: `${url}/${id}`, method: 'put' })
   switch (statusCode) {
-    case 204: return undefined
+    case 204: return true
     default: throw new UnexpectedError()
   }
 }
