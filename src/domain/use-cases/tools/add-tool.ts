b388041788eed05b8a99265225f731dbc20562d0
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-13 23:30:45 -0300

    feat: ensure addToolUseCase return true if HttpClient return 201

--- src/domain/use-cases/tools/add-tool.ts
+++ src/domain/use-cases/tools/add-tool.ts
@@ -3,13 +3,13 @@ import { UnexpectedError } from '../../errors'
 
 type Setup = (url: string, httpClient: HttpClient) => AddTool
 type Input = { name: string, description: string }
-type Output = void
+type Output = boolean
 export type AddTool = (input: Input) => Promise<Output>
 
 export const addToolUseCase: Setup = (url, httpClient) => async ({ description, name }) => {
   const { statusCode } = await httpClient.request({ url, method: 'post', body: { description, name } })
   switch (statusCode) {
-    case 200: return undefined
+    case 201: return true
     default: throw new UnexpectedError()
   }
 }
